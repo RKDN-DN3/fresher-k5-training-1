1bea7fddf302a67f6dbfb4106842818ecd78615a
tranhoanganh <hoanganhtran669@gmail.com>
2021-07-12 00:53:07 +0700

    commit -2

---
+++ src/component/table.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import {Form,Col,Row,Card, Table,InputGroup,Button} from 'react-bootstrap';
+ 
+function MusicTable(props){
+     return(
+      <Form>
+      <Row  className="show-grid">
+      <Col md={4}>
+        <Form.Group>
+          <InputGroup className="mb-3">
+            <Form.Control placeholder="Search item name" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+            <Button variant="info" id="button-addon2">Clean</Button>
+        </InputGroup>
+        </Form.Group>
+      </Col>
+      <Col md={3}>
+
+      </Col>
+      <Col md={5}>
+      <Form.Group>
+          <InputGroup className="mb-3">
+          <Button variant="success" id="button-addon2" className="select_r">Add Item</Button>
+          </InputGroup>
+        </Form.Group>
+      </Col>
+    </Row>
+      
+      <Row >
+        <Card className="card_width justify-content-end">
+        <Card.Header>Danh Sách Âm Nhạc</Card.Header>
+        <Card.Body>
+        <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Tên Bài Hát </th>
+                <th>Thể Hiện</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+                {props.musices.length >0 ?(
+                   props.musices.map(item=>{
+                       return(
+                       <tr>
+                       <td>{item.id}</td>
+                       <td>{item.musicName}</td>
+                       <td>{item.singer}</td>
+                       <td>
+                        <Button variant="warning"className="margin_item">Edit</Button>
+                        <Button variant="danger">delete</Button>
+                        </td>
+                       </tr>
+                       )
+                    })
+                ):(
+                    <tr>
+                        <td colSpan={4}>No music found</td>
+                    </tr>
+                )
+            }
+              
+            </tbody>
+          </Table>
+        </Card.Body>
+      </Card>
+      </Row >
+      </Form>
+     )
+ }
+
+ export default MusicTable
---
+++ src/data.js
@@ -0,0 +1,40 @@
+
+const musicData =[
+    {
+        id: 1,
+        musicName: "Bỏ em vào balo",
+        singer: "Tân Trần",
+        musician: "Tân Trần"
+    },
+    {
+        id: 2,
+        musicName: "Em của ngày hôm qua",
+        singer: "Sơn Tùng MTP",
+        musician: "Sơn Tùng MTP"
+    },
+    {
+        id: 3,
+        musicName: "Yêu 5",
+        singer: "Rhymastic",
+        musician: "Rhymastic"
+    },
+    {
+        id: 4,
+        musicName: "Nơi tình yêu bắt đầu",
+        singer: "Bùi Anh tuấn",
+        musician: "Tiến Minh"
+    },
+    {
+        id: 5,
+        musicName: "Nơi tình yêu kết thúc",
+        singer: "Bùi Anh tuấn",
+        musician: "Tiến Minh"
+    },
+    {
+        id: 6,
+        musicName: "CAFE KHÔNG ĐƯỜNG",
+        singer: "TKAN, BEAN",
+        musician: "JOMBIE"
+    }
+]; 
+export default musicData;
