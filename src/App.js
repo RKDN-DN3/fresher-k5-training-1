a9050eca652684100630a24c7c33919033f975f9
Trần Hoàng Anh <hoanganhtran669@gmail.com>
2021-07-12 09:37:28 +0700

    first commit develop

--- src/App.js
+++ src/App.js
@@ -4,21 +4,28 @@ import React,{useState} from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import musicData from './data';
 import MusicTable from './component/table';
-import AddMusic from './component/Add-music'
+import AddMusic from './component/Add-music';
 
 function App() {
  
   const [musices, setMusices] = useState(musicData);
   return (
-    <div className="App">
-      
+    <Router>
+    <div className="App"> 
       <div className="container"> 
         <h2>Project 01 - React CRUD</h2>
         <hr/>
-        <MusicTable musices ={musices}/>
+        <Switch>
+          <Route path="/" exact>
+            <MusicTable musices ={musices}/>
+          </Route>
+          <Route path="/add">
+            <AddMusic/>
+          </Route>
+        </Switch>
       </div>
     </div>
-    
+    </Router>
   );
 }
 
--- src/component/Add-music.js
+++ src/component/Add-music.js
@@ -1,59 +1,30 @@
-import React,{useState} from 'react';
-import {Row, Col,Form,Button} from'react-bootstrap';
+    import React from 'react';
+    import {Row, Col,Form,Button, Card} from'react-bootstrap';
+    import './Add-music.css';
 
-function AddMusic(){
-    return(
-        <Form>
-            <Row className="mb-3">
-                <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                </Form.Group>
+    function AddMusic(){
+        return(
+            console.log("abc"),
+            <Row style ={{width:'70%', alignItems: 'center'}}>
+            <Card  >
+                <Card.Header>
+                    <h4>Thêm bài hát mới</h4>
+                </Card.Header >
+                <Form >
+                    <Row className="mb-3">
+                        <Form.Group as={Col} controlId="formGridEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
 
-                <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
+                        <Form.Group as={Col} controlId="formGridPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                    </Row>
+                </Form>
+            </Card>
             </Row>
-
-            <Form.Group className="mb-3" controlId="formGridAddress1">
-                <Form.Label>Address</Form.Label>
-                <Form.Control placeholder="1234 Main St" />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formGridAddress2">
-                <Form.Label>Address 2</Form.Label>
-                <Form.Control placeholder="Apartment, studio, or floor" />
-            </Form.Group>
-
-            <Row className="mb-3">
-                <Form.Group as={Col} controlId="formGridCity">
-                <Form.Label>City</Form.Label>
-                <Form.Control />
-                </Form.Group>
-
-                <Form.Group as={Col} controlId="formGridState">
-                <Form.Label>State</Form.Label>
-                <Form.Select defaultValue="Choose...">
-                    <option>Choose...</option>
-                    <option>...</option>
-                </Form.Select>
-                </Form.Group>
-
-                <Form.Group as={Col} controlId="formGridZip">
-                <Form.Label>Zip</Form.Label>
-                <Form.Control />
-                </Form.Group>
-            </Row>
-
-            <Form.Group className="mb-3" id="formGridCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
-    )
-}
-export default AddMusic;
+        )
+    }
+    export default AddMusic;
